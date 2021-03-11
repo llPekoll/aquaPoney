ae018fbd90f7e7ce11a825a96638dabcdb0db720
peko <yoyo.mepa@gmail.com>
2021-03-11 23:30:48 +0100

    got all working

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
-	export let name: string;
-	
 	import  Chart  from "./Chart.svelte";
-
-
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Aquaponics!</h1>
 <Chart />
-
 </main>
 
 <style>
