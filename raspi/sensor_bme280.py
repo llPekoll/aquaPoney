bb0d5bf6b84132b18fb84a6bc4a9bd3ddf42f3d6
peko <yoyo.mepa@gmail.com>
2021-03-12 11:28:41 +0100

    got all working

--- raspi/sensor_bme280.py
+++ raspi/sensor_bme280.py
@@ -27,14 +27,20 @@ def get_bme280_values():
     return vals
 
 def push_bme280_values():
+    print(1)
     datas = {
         "temperature":f"{bme280.temperature:0.1f}",
         "humidity": f"{bme280.relative_humidity:0.1f}",
         "pressure": f"{bme280.pressure:0.1f}",
         "altitude": f"{bme280.altitude:0.2f}",
     }
+    print(2)
     SERVER_IP = os.environ.get("SERVER_IP")
+    print(3)
     APIKEY = f"{os.environ.get('API_KEY_NAME')}={os.environ.get('API_KEY')}"
+    print(4)
     req = f"https://{SERVER_IP}/bme280?{APIKEY}"
-    requests.post(req, data = {'key':'value'})
+    print(req)
+    ret = requests.post(req, data = {'key':'value'})
+    print(ret)
 
