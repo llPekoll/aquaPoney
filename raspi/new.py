a55accf419ca24ef80d7feb0c9d87e1343d1dd85
peko <yoyo.mepa@gmail.com>
2021-04-05 17:27:21 +0200

    read raspi

--- raspi/new.py
+++ raspi/new.py
@@ -14,22 +14,3 @@ while True:
     number = ser.read()
     if number != b'':
         logging.info(number)
-        
-
-# while True:
-#     time.sleep(1) # in sec
-#     datas = mirror.readline()
-#     logging.info(datas)
-# #!/usr/bin/env python3
-# import serial
-# import time
-
-# if __name__ == '__main__':
-#     ser = serial.Serial(port, 115200, timeout=1)
-#     ser.flush()
-
-#     while True:
-#         ser.write(b"Hello from Raspberry Pi!\n")
-#         line = ser.readline().decode('utf-8').rstrip()
-#         print(line)
-#         time.sleep(1)
